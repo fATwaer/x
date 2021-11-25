7bd749ad37ba0d6a38175dccdbf0cb9d13e0af08
fatwaer <fatwaer@gmail.com>
2021-11-25 13:32:35 +0800

    init

---
+++ server/main.go
@@ -0,0 +1,105 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// Package main implements a server for Greeter service.
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "rpc-mocker/helloworld"
+
+	"reflect"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+func createQuery(q interface{}) {
+	if reflect.ValueOf(q).Kind() == reflect.Struct {
+		t := reflect.TypeOf(q).Name()
+		query := fmt.Sprintf("insert into %s values(", t)
+		v := reflect.ValueOf(q)
+		for i := 0; i < v.NumField(); i++ {
+			switch v.Field(i).Kind() {
+			case reflect.Int:
+				if i == 0 {
+					query = fmt.Sprintf("%s%d", query, v.Field(i).Int())
+				} else {
+					query = fmt.Sprintf("%s, %d", query, v.Field(i).Int())
+				}
+			case reflect.String:
+				if i == 0 {
+					query = fmt.Sprintf("%s\"%s\"", query, v.Field(i).String())
+				} else {
+					query = fmt.Sprintf("%s, \"%s\"", query, v.Field(i).String())
+				}
+			default:
+				fmt.Println("Unsupported type")
+				continue
+			}
+		}
+		query = fmt.Sprintf("%s)", query)
+		fmt.Println(query)
+		return
+
+	}
+	fmt.Println("unsupported type")
+}
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+type server2 struct {
+	pb.UnimplementedGreeter2Server
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func (s *server2) SayHello(ctx context.Context, in *pb.HelloRequest2) (*pb.HelloReply2, error) {
+	log.Printf("Received: %v", in.Id)
+	return &pb.HelloReply2{Rid: 42}, nil
+}
+
+func main() {
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	pb.RegisterGreeter2Server(s, &server2{})
+	createQuery(server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
